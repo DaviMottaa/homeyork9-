5a3b07ad84bd0c7f82f8ddcb624c8e395870af2f
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-11 20:13:58 -0300

    Add files via upload

---
+++ Davi_21079.py
@@ -0,0 +1,37 @@
+#Question2
+for item in range(1,11):
+    print(item)
+
+#Question3
+print("3**************************")
+for item in range(1,11):
+    if item >5:
+        print(item)
+
+#Question4
+print("4**************************")
+for item in range(1,11):
+    if item <=3:
+        print(item)
+
+#Question5
+print("5**************************")
+for item in range(1,11):
+    if item <=4 or item >8:
+        print(item)
+
+#Question6
+print("6**************************")
+for item in range(1,11):
+    if item >3 and item <6:
+        print(item)
+
+#Question7
+print("7**************************")
+numbers=list(range(2,13,2))
+print(numbers)
+
+#Question8
+print("8**************************")
+for item in numbers:
+    print(item)
